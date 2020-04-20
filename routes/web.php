7eb3f4e1cc3b87bd5bc4f82dce0164803cdfd742
maxlyh93 <maxlyh93@gmail.com>
2020-04-20 20:56:59 +0800

    multi guard authentication

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes([ 'verify' => true ]);
+
+
+/* --------------------- Common/User Routes END -------------------------------- */
+
+/* ----------------------- Admin Routes START -------------------------------- */
+
+Route::prefix('/admin')->name('admin.')->namespace('Admin')->group(function(){
+    
+    /**
+     * Admin Auth Route(s)
+     */
+    Route::namespace('Auth')->group(function(){
+        
+        //Login Routes
+        Route::get('/login','LoginController@showLoginForm')->name('login');
+        Route::post('/login','LoginController@login');
+        Route::post('/logout','LoginController@logout')->name('logout');
+
+        //Register Routes
+        // Route::get('/register','RegisterController@showRegistrationForm')->name('register');
+        // Route::post('/register','RegisterController@register');
+
+        //Forgot Password Routes
+        Route::get('/password/reset','ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        Route::post('/password/email','ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+
+        //Reset Password Routes
+        Route::get('/password/reset/{token}','ResetPasswordController@showResetForm')->name('password.reset');
+        Route::post('/password/reset','ResetPasswordController@reset')->name('password.update');
+    });
+
+    Route::get('/dashboard','HomeController@index')->name('home');
+
+    //Put all of your admin routes here...
+
+});
+
+/* ----------------------- Admin Routes END -------------------------------- */
+  
